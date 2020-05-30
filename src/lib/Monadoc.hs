a54276766b9815a02c11201e2cd3100227ac414b
Taylor Fausak <taylor@fausak.me>
2020-05-30 10:55:14 -0400

    Add a basic HTTP server

--- src/lib/Monadoc.hs
+++ src/lib/Monadoc.hs
@@ -1,4 +1,10 @@
 module Monadoc ( monadoc ) where
 
+import qualified Data.ByteString.Lazy as LazyByteString
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.HTTP.Types as Http
+
 monadoc :: IO ()
-monadoc = pure ()
+monadoc = Warp.run 8080 $ \ _ respond ->
+  respond $ Wai.responseLBS Http.ok200 [] LazyByteString.empty
