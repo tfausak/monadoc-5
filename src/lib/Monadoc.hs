ee0f778ceb567657500586aa7cfdf837b415c448
Taylor Fausak <taylor@fausak.me>
2020-05-30 10:59:30 -0400

    Add a SQLite connection

--- src/lib/Monadoc.hs
+++ src/lib/Monadoc.hs
@@ -4,7 +4,9 @@ import qualified Data.ByteString.Lazy as LazyByteString
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Network.HTTP.Types as Http
+import qualified Database.SQLite.Simple as Sql
 
 monadoc :: IO ()
-monadoc = Warp.run 8080 $ \ _ respond ->
-  respond $ Wai.responseLBS Http.ok200 [] LazyByteString.empty
+monadoc = Sql.withConnection ":memory:" $ \ _ ->
+  Warp.run 8080 $ \ _ respond ->
+    respond $ Wai.responseLBS Http.ok200 [] LazyByteString.empty
