beb8fce07f5753268d53dbec39d201d29ce97ebd
Taylor Fausak <taylor@fausak.me>
2020-06-05 15:45:27 -0400

    Move worker into its own namespace

--- src/lib/Monadoc.hs
+++ src/lib/Monadoc.hs
@@ -5,17 +5,11 @@ module Monadoc
   )
 where
 
-import qualified Codec.Archive.Tar as Tar
-import qualified Codec.Compression.GZip as Gzip
-import qualified Control.Concurrent as Concurrent
 import qualified Control.Concurrent.Async as Async
 import qualified Control.Monad as Monad
 import qualified Control.Monad.Catch as Exception
 import qualified Control.Monad.Trans.Class as Trans
 import qualified Control.Monad.Trans.Reader as Reader
-import qualified Crypto.Hash as Crypto
-import qualified Data.ByteString as ByteString
-import qualified Data.ByteString.Lazy as LazyByteString
 import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
 import qualified Data.Pool as Pool
@@ -29,24 +23,15 @@ import qualified Monadoc.Data.Options as Options
 import qualified Monadoc.Data.Version as Version
 import qualified Monadoc.Server.Main as Server
 import qualified Monadoc.Type.App as App
-import qualified Monadoc.Type.Binary as Binary
 import qualified Monadoc.Type.Config as Config
 import qualified Monadoc.Type.Context as Context
-import qualified Monadoc.Type.Etag as Etag
 import qualified Monadoc.Type.Migration as Migration
 import qualified Monadoc.Type.MigrationMismatch as MigrationMismatch
-import qualified Monadoc.Type.Sha256 as Sha256
-import qualified Monadoc.Type.Size as Size
-import qualified Monadoc.Type.WithCallStack as WithCallStack
-import qualified Network.HTTP.Client as Client
-import qualified Network.HTTP.Types as Http
-import qualified Network.HTTP.Types.Header as Http
+import qualified Monadoc.Worker.Main as Worker
 import qualified System.Console.GetOpt as GetOpt
 import qualified System.Environment as Environment
 import qualified System.Exit as Exit
-import qualified System.FilePath as FilePath
 import qualified System.IO as IO
-import qualified System.IO.Unsafe as Unsafe
 
 monadoc :: Stack.HasCallStack => IO ()
 monadoc = do
@@ -60,7 +45,7 @@ monadoc = do
     ]
   context <- Context.fromConfig config
   runApp context migrate
-  Async.race_ (runApp context Server.run) (runApp context worker)
+  Async.race_ (runApp context Server.run) (runApp context Worker.run)
 
 migrate :: App.App ()
 migrate = withConnection $ \connection -> Trans.lift $ do
@@ -129,129 +114,3 @@ getConfig = do
       Just hash -> "-" <> hash
     Exit.exitSuccess
   pure config
-
-worker :: App.App ()
-worker = Monad.forever $ do
-  Console.info "updating hackage index"
-  let url = "https://hackage.haskell.org/01-index.tar.gz"
-  result <- withConnection $ \connection -> Trans.lift $ Sql.query
-    connection
-    (query "select etag, sha256 from cache where url = ?")
-    [url]
-  contents <- case result of
-    [] -> do
-      Console.info "index is not cached"
-      request <- Client.parseRequest url
-      manager <- Reader.asks Context.manager
-      response <- Trans.lift $ Client.httpLbs request manager
-      Monad.when (Client.responseStatus response /= Http.ok200)
-        . WithCallStack.throw
-        . userError
-        $ show response
-      let
-        body = LazyByteString.toStrict $ Client.responseBody response
-        sha256 = Sha256.fromDigest $ Crypto.hash body
-        etag =
-          Etag.fromByteString
-            . Maybe.fromMaybe ByteString.empty
-            . lookup Http.hETag
-            $ Client.responseHeaders response
-      Console.info "got index, caching response"
-      withConnection $ \connection -> Trans.lift $ do
-        Sql.execute
-          connection
-          (query
-            "insert into blobs (octets, sha256, size) \
-            \ values (?, ?, ?) on conflict (sha256) do nothing"
-          )
-          (Binary.fromByteString body, sha256, Size.fromInt $ ByteString.length body)
-        Sql.execute
-          connection
-          (query
-            "insert into cache (etag, sha256, url) values (?, ?, ?) \
-            \ on conflict (url) do update set \
-            \ etag = excluded.etag, sha256 = excluded.sha256"
-          )
-          (etag, sha256, url)
-      pure body
-    (etag, sha256) : _ -> do
-      Console.info "index is cached"
-      request <- addRequestHeader Http.hIfNoneMatch (Etag.toByteString etag)
-        <$> Client.parseRequest url
-      manager <- Reader.asks Context.manager
-      response <- Trans.lift $ Client.httpLbs request manager
-      case Http.statusCode $ Client.responseStatus response of
-        200 -> do
-          Console.info "index has changed"
-          let
-            body = LazyByteString.toStrict $ Client.responseBody response
-            newSha256 = Sha256.fromDigest $ Crypto.hash body
-            newEtag =
-              Etag.fromByteString
-                . Maybe.fromMaybe ByteString.empty
-                . lookup Http.hETag
-                $ Client.responseHeaders response
-          Console.info "got index, caching response"
-          withConnection $ \connection -> Trans.lift $ do
-            Sql.execute
-              connection
-              (query
-                "insert into blobs (octets, sha256, size) \
-                \ values (?, ?, ?) on conflict (sha256) do nothing"
-              )
-              ( Binary.fromByteString body
-              , newSha256
-              , Size.fromInt $ ByteString.length body
-              )
-            Sql.execute
-              connection
-              (query
-                "insert into cache (etag, sha256, url) values (?, ?, ?) \
-                \ on conflict (url) do update set \
-                \ etag = excluded.etag, sha256 = excluded.sha256"
-              )
-              (newEtag, newSha256, url)
-          pure body
-        304 -> do
-          Console.info "index has not changed"
-          rows <- withConnection $ \connection -> Trans.lift $ Sql.query
-            connection
-            (query "select octets from blobs where sha256 = ?")
-            [sha256 :: Sha256.Sha256]
-          case rows of
-            [] -> WithCallStack.throw $ userError "missing index blob"
-            row : _ -> pure . Binary.toByteString $ Sql.fromOnly row
-        _ -> WithCallStack.throw . userError $ show response
-  Console.info $ "index size: " <> show (ByteString.length contents)
-  Console.info
-    . mappend "index entry count: "
-    . show
-    . length
-    . Tar.foldEntries
-        (\entry entries -> case Tar.entryContent entry of
-          Tar.NormalFile _contents _size ->
-            case FilePath.takeExtension $ Tar.entryPath entry of
-              "" -> entry : entries -- preferred-versions
-              ".cabal" -> entry : entries
-              ".json" -> entries -- ignore
-              _ ->
-                Unsafe.unsafePerformIO . WithCallStack.throw . userError $ show
-                  entry
-          _ -> Unsafe.unsafePerformIO . WithCallStack.throw . userError $ show
-            entry
-        )
-        []
-        (Unsafe.unsafePerformIO . WithCallStack.throw)
-    . Tar.read
-    . Gzip.decompress
-    $ LazyByteString.fromStrict contents
-  Trans.lift $ Concurrent.threadDelay 60000000
-
-addRequestHeader
-  :: Http.HeaderName
-  -> ByteString.ByteString
-  -> Client.Request
-  -> Client.Request
-addRequestHeader name value request = request
-  { Client.requestHeaders = (name, value) : Client.requestHeaders request
-  }
---
+++ src/lib/Monadoc/Worker/Main.hs
@@ -0,0 +1,167 @@
+{-# LANGUAGE RankNTypes #-}
+
+module Monadoc.Worker.Main
+  ( run
+  )
+where
+
+import qualified Codec.Archive.Tar as Tar
+import qualified Codec.Compression.GZip as Gzip
+import qualified Control.Concurrent as Concurrent
+import qualified Control.Monad as Monad
+import qualified Control.Monad.Trans.Class as Trans
+import qualified Control.Monad.Trans.Reader as Reader
+import qualified Crypto.Hash as Crypto
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Lazy as LazyByteString
+import qualified Data.Maybe as Maybe
+import qualified Data.Pool as Pool
+import qualified Data.Text as Text
+import qualified Database.SQLite.Simple as Sql
+import qualified Monadoc.Console as Console
+import qualified Monadoc.Type.App as App
+import qualified Monadoc.Type.Binary as Binary
+import qualified Monadoc.Type.Context as Context
+import qualified Monadoc.Type.Etag as Etag
+import qualified Monadoc.Type.Sha256 as Sha256
+import qualified Monadoc.Type.Size as Size
+import qualified Monadoc.Type.WithCallStack as WithCallStack
+import qualified Network.HTTP.Client as Client
+import qualified Network.HTTP.Types as Http
+import qualified Network.HTTP.Types.Header as Http
+import qualified System.FilePath as FilePath
+import qualified System.IO.Unsafe as Unsafe
+
+withConnection :: (Sql.Connection -> App.App a) -> App.App a
+withConnection app = do
+  pool <- Reader.asks Context.pool
+  Pool.withResource pool app
+
+query :: String -> Sql.Query
+query = Sql.Query . Text.pack
+
+run :: App.App ()
+run = Monad.forever $ do
+  Console.info "updating hackage index"
+  let url = "https://hackage.haskell.org/01-index.tar.gz"
+  result <- withConnection $ \connection -> Trans.lift $ Sql.query
+    connection
+    (query "select etag, sha256 from cache where url = ?")
+    [url]
+  contents <- case result of
+    [] -> do
+      Console.info "index is not cached"
+      request <- Client.parseRequest url
+      manager <- Reader.asks Context.manager
+      response <- Trans.lift $ Client.httpLbs request manager
+      Monad.when (Client.responseStatus response /= Http.ok200)
+        . WithCallStack.throw
+        . userError
+        $ show response
+      let
+        body = LazyByteString.toStrict $ Client.responseBody response
+        sha256 = Sha256.fromDigest $ Crypto.hash body
+        etag =
+          Etag.fromByteString
+            . Maybe.fromMaybe ByteString.empty
+            . lookup Http.hETag
+            $ Client.responseHeaders response
+      Console.info "got index, caching response"
+      withConnection $ \connection -> Trans.lift $ do
+        Sql.execute
+          connection
+          (query
+            "insert into blobs (octets, sha256, size) \
+            \ values (?, ?, ?) on conflict (sha256) do nothing"
+          )
+          (Binary.fromByteString body, sha256, Size.fromInt $ ByteString.length body)
+        Sql.execute
+          connection
+          (query
+            "insert into cache (etag, sha256, url) values (?, ?, ?) \
+            \ on conflict (url) do update set \
+            \ etag = excluded.etag, sha256 = excluded.sha256"
+          )
+          (etag, sha256, url)
+      pure body
+    (etag, sha256) : _ -> do
+      Console.info "index is cached"
+      request <- addRequestHeader Http.hIfNoneMatch (Etag.toByteString etag)
+        <$> Client.parseRequest url
+      manager <- Reader.asks Context.manager
+      response <- Trans.lift $ Client.httpLbs request manager
+      case Http.statusCode $ Client.responseStatus response of
+        200 -> do
+          Console.info "index has changed"
+          let
+            body = LazyByteString.toStrict $ Client.responseBody response
+            newSha256 = Sha256.fromDigest $ Crypto.hash body
+            newEtag =
+              Etag.fromByteString
+                . Maybe.fromMaybe ByteString.empty
+                . lookup Http.hETag
+                $ Client.responseHeaders response
+          Console.info "got index, caching response"
+          withConnection $ \connection -> Trans.lift $ do
+            Sql.execute
+              connection
+              (query
+                "insert into blobs (octets, sha256, size) \
+                \ values (?, ?, ?) on conflict (sha256) do nothing"
+              )
+              ( Binary.fromByteString body
+              , newSha256
+              , Size.fromInt $ ByteString.length body
+              )
+            Sql.execute
+              connection
+              (query
+                "insert into cache (etag, sha256, url) values (?, ?, ?) \
+                \ on conflict (url) do update set \
+                \ etag = excluded.etag, sha256 = excluded.sha256"
+              )
+              (newEtag, newSha256, url)
+          pure body
+        304 -> do
+          Console.info "index has not changed"
+          rows <- withConnection $ \connection -> Trans.lift $ Sql.query
+            connection
+            (query "select octets from blobs where sha256 = ?")
+            [sha256 :: Sha256.Sha256]
+          case rows of
+            [] -> WithCallStack.throw $ userError "missing index blob"
+            row : _ -> pure . Binary.toByteString $ Sql.fromOnly row
+        _ -> WithCallStack.throw . userError $ show response
+  Console.info $ "index size: " <> show (ByteString.length contents)
+  Console.info
+    . mappend "index entry count: "
+    . show
+    . length
+    . Tar.foldEntries
+        (\entry entries -> case Tar.entryContent entry of
+          Tar.NormalFile _contents _size ->
+            case FilePath.takeExtension $ Tar.entryPath entry of
+              "" -> entry : entries -- preferred-versions
+              ".cabal" -> entry : entries
+              ".json" -> entries -- ignore
+              _ ->
+                Unsafe.unsafePerformIO . WithCallStack.throw . userError $ show
+                  entry
+          _ -> Unsafe.unsafePerformIO . WithCallStack.throw . userError $ show
+            entry
+        )
+        []
+        (Unsafe.unsafePerformIO . WithCallStack.throw)
+    . Tar.read
+    . Gzip.decompress
+    $ LazyByteString.fromStrict contents
+  Trans.lift $ Concurrent.threadDelay 60000000
+
+addRequestHeader
+  :: Http.HeaderName
+  -> ByteString.ByteString
+  -> Client.Request
+  -> Client.Request
+addRequestHeader name value request = request
+  { Client.requestHeaders = (name, value) : Client.requestHeaders request
+  }
