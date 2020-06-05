425ad584e3b08cf7cd231f9a9d07266f6dde0fd3
Taylor Fausak <taylor@fausak.me>
2020-06-05 15:59:59 -0400

    Stop using OverloadedStrings

--- src/test/Monadoc/CabalSpec.hs
+++ src/test/Monadoc/CabalSpec.hs
@@ -1,21 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Monadoc.CabalSpec
   ( spec
   )
 where
 
 import qualified Data.Either as Either
+import qualified Data.ByteString.Char8 as ByteString
 import qualified Monadoc.Cabal as Monadoc
 import qualified Test.Hspec as Hspec
 
 spec :: Hspec.Spec
 spec = do
 
-  Hspec.it "parses an empty package"
-    $ Monadoc.parse "name:x\nversion:0"
-    `Hspec.shouldSatisfy` Either.isRight
+  Hspec.it "parses an empty package" $ do
+    Monadoc.parse (ByteString.pack "name:x\nversion:0") `Hspec.shouldSatisfy` Either.isRight
 
-  Hspec.it "fails to parse an invalid package"
-    $ Monadoc.parse ""
-    `Hspec.shouldSatisfy` Either.isLeft
+  Hspec.it "fails to parse an invalid package" $ do
+    Monadoc.parse ByteString.empty `Hspec.shouldSatisfy` Either.isLeft
--- src/test/Monadoc/GhcSpec.hs
+++ src/test/Monadoc/GhcSpec.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Monadoc.GhcSpec
   ( spec
   )
 where
 
 import qualified Data.Either as Either
+import qualified Data.ByteString.Char8 as ByteString
 import qualified Monadoc.Ghc as Monadoc
 import qualified Test.Hspec as Hspec
 
@@ -13,9 +12,9 @@ spec :: Hspec.Spec
 spec = do
 
   Hspec.it "parses an empty module" $ do
-    result <- Monadoc.parse "" "module M where"
+    result <- Monadoc.parse "" $ ByteString.pack "module M where"
     result `Hspec.shouldSatisfy` Either.isRight
 
   Hspec.it "fails to parse an invalid module" $ do
-    result <- Monadoc.parse "" "module"
+    result <- Monadoc.parse "" $ ByteString.pack "module"
     result `Hspec.shouldSatisfy` Either.isLeft
