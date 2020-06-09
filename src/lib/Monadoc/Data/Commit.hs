e5ffc9bdfe94f0bebf0eed51843cdeed68dec36c
Taylor Fausak <taylor@fausak.me>
2020-06-09 12:09:00 -0400

    Try to set Git commit hash using CPP

--- src/lib/Monadoc/Data/Commit.hs
+++ src/lib/Monadoc/Data/Commit.hs
@@ -1,16 +1,21 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE CPP #-}
 
 module Monadoc.Data.Commit
   ( hash
   )
 where
 
-import qualified GitHash
-
-gitInfo :: Either String GitHash.GitInfo
-gitInfo = $$( GitHash.tGitInfoCwdTry )
-
 -- | The Git commit hash this package was built at, if available. Like the
 -- version number, you'll probably only need this for diagnostics.
 hash :: Maybe String
-hash = either (const Nothing) (Just . GitHash.giHash) gitInfo
+
+#ifdef MONADOC_COMMIT_HASH
+
+#define stringize(x) #x
+hash = Just stringize(MONADOC_COMMIT_HASH)
+
+#else
+
+hash = Nothing
+
+#endif
