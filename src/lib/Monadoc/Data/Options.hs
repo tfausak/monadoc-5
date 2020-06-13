31169aa42cfc0bf316ad8a9caa72cdbc609c69c9
Taylor Fausak <taylor@fausak.me>
2020-06-13 09:06:16 -0400

    Wire up the OAuth client ID and secret

--- src/lib/Monadoc/Data/Options.hs
+++ src/lib/Monadoc/Data/Options.hs
@@ -8,34 +8,71 @@ import qualified Monadoc.Type.Config as Config
 import qualified System.Console.GetOpt as GetOpt
 import qualified Text.Read as Read
 
+type Option = GetOpt.OptDescr (Config.Config -> Either String Config.Config)
+
 -- | Collection of command-line options. Run the app with @--help@ to see what
 -- they are.
-options :: [GetOpt.OptDescr (Config.Config -> Either String Config.Config)]
+options :: [Option]
 options =
-  [ option
-      []
-      ["database"]
-      "sets the database file (defaults to monadoc.sqlite3)"
+  [ clientIdOption
+  , clientSecretOption
+  , databaseOption
+  , helpOption
+  , hostOption
+  , portOption
+  , urlOption
+  , versionOption
+  ]
+
+clientIdOption :: Option
+clientIdOption =
+  option [] ["client-id"] "sets the client ID (defaults to 235ce...)"
+    . argument "STRING"
+    $ \clientId config -> Right config { Config.clientId = clientId }
+
+clientSecretOption :: Option
+clientSecretOption =
+  option [] ["client-secret"] "sets the client secret (defaults to 48e20...)"
+    . argument "STRING"
+    $ \clientSecret config ->
+        Right config { Config.clientSecret = clientSecret }
+
+databaseOption :: Option
+databaseOption =
+  option [] ["database"] "sets the database file (defaults to monadoc.sqlite3)"
     . argument "FILE"
     $ \database config -> Right config { Config.database = database }
-  , option ['h'] ["help"] "shows the help and exits"
-    . GetOpt.NoArg
-    $ \config -> Right config { Config.help = True }
-  , option [] ["host"] "sets the server host (defaults to 127.0.0.1)"
+
+helpOption :: Option
+helpOption =
+  option ['h'] ["help"] "shows the help and exits" . GetOpt.NoArg $ \config ->
+    Right config { Config.help = True }
+
+hostOption :: Option
+hostOption =
+  option [] ["host"] "sets the server host (defaults to 127.0.0.1)"
     . argument "STRING"
     $ \host config -> Right config { Config.host = String.fromString host }
-  , option [] ["port"] "sets the server port (defaults to 4444)"
+
+portOption :: Option
+portOption =
+  option [] ["port"] "sets the server port (defaults to 4444)"
     . argument "NUMBER"
     $ \rawPort config -> case Read.readMaybe rawPort of
         Nothing -> Left $ "invalid port: " <> show rawPort
         Just port -> Right config { Config.port = port }
-  , option [] ["url"] "sets the base URL (defaults to http://localhost:4444)"
+
+urlOption :: Option
+urlOption =
+  option [] ["url"] "sets the base URL (defaults to http://localhost:4444)"
     . argument "URL"
     $ \url config -> Right config { Config.url = url }
-  , option ['v'] ["version"] "shows the version number and exits"
+
+versionOption :: Option
+versionOption =
+  option ['v'] ["version"] "shows the version number and exits"
     . GetOpt.NoArg
     $ \config -> Right config { Config.version = True }
-  ]
 
 option
   :: String -> [String] -> String -> GetOpt.ArgDescr a -> GetOpt.OptDescr a
--- src/lib/Monadoc/Type/Config.hs
+++ src/lib/Monadoc/Type/Config.hs
@@ -9,8 +9,17 @@ import qualified Network.Wai.Handler.Warp as Warp
 -- | Application configuration. This contains all the stuff necessary to start
 -- things up. Descriptions and defaults are available by running the executable
 -- with @--help@.
+--
+-- It is expected that each field here will have a corresponding option defined
+-- in "Monadoc.Data.Options".
 data Config = Config
-  { database :: FilePath
+  { clientId :: String
+  -- ^ The client ID for the GitHub OAuth application. Make sure this goes with
+  -- the secret.
+  , clientSecret :: String
+  -- ^ The client secret for the GitHub OAuth application. Make sure this goes
+  -- with the ID.
+  , database :: FilePath
   -- ^ The path to the database file. To use an in-memory databse, set this to
   -- either the empty string or @":memory:"@.
   , help :: Bool
@@ -21,7 +30,7 @@ data Config = Config
   -- ^ The port to bind on.
   , url :: String
   -- ^ The base URL that the site is available at. Be sure to change this if
-  -- you change the host or port.
+  -- you change the port.
   , version :: Bool
   -- ^ Whether or not to show the version number.
   } deriving (Eq, Show)
@@ -29,7 +38,9 @@ data Config = Config
 -- | The default config. These values are optimized for development.
 initial :: Config
 initial = Config
-  { database = "monadoc.sqlite3"
+  { clientId = "235ce8c873f4ed90905c"
+  , clientSecret = "48e202a2b3aa30ad2a4e844f77b7d10807ab1deb"
+  , database = "monadoc.sqlite3"
   , help = False
   , host = "127.0.0.1"
   , port = 4444
