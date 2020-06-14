fffcf63044cf7fe397c7f5ff905d840833f7abe7
Taylor Fausak <taylor@fausak.me>
2020-06-14 15:29:06 -0400

    Clean up the GitHub callback

--- src/lib/Monadoc/Handler/GitHubCallback.hs
+++ src/lib/Monadoc/Handler/GitHubCallback.hs
@@ -3,15 +3,18 @@ module Monadoc.Handler.GitHubCallback
   )
 where
 
+import qualified Control.Monad as Monad
 import qualified Control.Monad.Trans.Class as Trans
 import qualified Control.Monad.Trans.Reader as Reader
 import qualified Data.Aeson as Aeson
+import qualified Data.ByteString as ByteString
 import qualified Data.ByteString.Lazy as LazyByteString
 import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
+import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 import qualified Data.UUID as Uuid
 import qualified GHC.Stack as Stack
-import qualified Monadoc.Console as Console
 import qualified Monadoc.Server.Common as Common
 import qualified Monadoc.Server.Settings as Settings
 import qualified Monadoc.Type.App as App
@@ -31,66 +34,84 @@ import qualified System.Random as Random
 
 handle :: Stack.HasCallStack => App.App Wai.Request Wai.Response
 handle = do
+  maybeCode <- getCode
+  code <- case maybeCode of
+    Nothing -> WithCallStack.throw $ userError "no code"
+    Just code -> pure code
+
+  maybeToken <- getToken code
+  token <- case maybeToken of
+    Nothing -> WithCallStack.throw $ userError "no token"
+    Just token -> pure token
+
+  maybeUser <- getUser token
+  user <- case maybeUser of
+    Nothing -> WithCallStack.throw $ userError "no user"
+    Just user -> pure user
+
+  maybeGuid <- upsertUser token user
+  guid <- case maybeGuid of
+    Nothing -> WithCallStack.throw $ userError "no guid"
+    Just guid -> pure guid
+
+  cookie <- makeCookie guid
+  config <- Reader.asks Context.config
+  pure
+    . Common.statusResponse Http.found302
+    . Map.insert Http.hLocation "/"
+    . Map.insert Http.hSetCookie cookie
+    $ Common.defaultHeaders config
+
+getCode :: App.App Wai.Request (Maybe ByteString.ByteString)
+getCode =
+  Reader.asks $ Monad.join . lookup "code" . Wai.queryString . Context.request
+
+getToken :: ByteString.ByteString -> App.App request (Maybe Text.Text)
+getToken code = do
   context <- Reader.ask
-
-  -- Get information from request.
-  Console.info . show $ Context.request context
-  body <- Trans.lift . Wai.lazyRequestBody $ Context.request context
-  Console.info $ show body
-  code <- case lookup "code" . Wai.queryString $ Context.request context of
-    Just (Just x) -> pure x
-    _ -> WithCallStack.throw $ userError "no code"
-
-  -- Validate code with GitHub.
-  let config = Context.config context
-  request1 <- do
-    initial <- Client.parseUrlThrow
-      "https://github.com/login/oauth/access_token"
-    pure $ Client.urlEncodedBody
+  initialRequest <- Client.parseUrlThrow
+    "https://github.com/login/oauth/access_token"
+  let
+    config = Context.config context
+    request = Client.urlEncodedBody
       [ ("client_id", Utf8.fromString $ Config.clientId config)
       , ("client_secret", Utf8.fromString $ Config.clientSecret config)
       , ("code", code)
       ]
-      initial
-  let manager = Context.manager context
-  response1 <- Trans.lift $ Client.httpLbs request1 manager
-  Console.info $ show response1
-  token <-
-    case
-      lookup "access_token"
-      . Http.parseQueryText
-      . LazyByteString.toStrict
-      $ Client.responseBody response1
-    of
-      Just (Just x) -> pure x
-      _ -> WithCallStack.throw $ userError "no access token"
+      initialRequest
+  response <- Trans.lift . Client.httpLbs request $ Context.manager context
+  pure
+    . Monad.join
+    . lookup "access_token"
+    . Http.parseQueryText
+    . LazyByteString.toStrict
+    $ Client.responseBody response
 
-  -- Get user information.
-  request2 <- do
-    initial <- Client.parseUrlThrow "https://api.github.com/user"
-    pure initial
+getUser :: Text.Text -> App.App request (Maybe GHUser.User)
+getUser token = do
+  context <- Reader.ask
+  initialRequest <- Client.parseUrlThrow "https://api.github.com/user"
+  let
+    request = initialRequest
       { Client.requestHeaders =
         [ (Http.hAuthorization, "Bearer " <> Text.encodeUtf8 token)
         , (Http.hUserAgent, Settings.serverName)
         ]
       }
-  response2 <- Trans.lift $ Client.httpLbs request2 manager
-  Console.info $ show response2
-  ghUser <-
-    either (WithCallStack.throw . userError) pure
-    . Aeson.eitherDecode
-    $ Client.responseBody response2
+  response <- Trans.lift . Client.httpLbs request $ Context.manager context
+  pure . Aeson.decode $ Client.responseBody response
 
-  -- Store user information.
-  newGuid <- Trans.lift $ Random.getStdRandom Guid.random
+upsertUser :: Text.Text -> GHUser.User -> App.App request (Maybe Guid.Guid)
+upsertUser token ghUser = do
+  guid <- Trans.lift $ Random.getStdRandom Guid.random
   let
     user = User.User
-      { User.guid = newGuid
+      { User.guid = guid
       , User.id = GHUser.id ghUser
       , User.login = GHUser.login ghUser
       , User.token = token
       }
-  guid <- App.withConnection $ \connection ->
+  App.withConnection $ \connection ->
     Trans.lift . Sql.withTransaction connection $ do
       Sql.execute
         connection
@@ -102,21 +123,14 @@ handle = do
         connection
         "select guid from users where id = ?"
         [User.id user]
-      case rows of
-        [] -> WithCallStack.throw $ userError "no user"
-        row : _ -> pure $ Sql.fromOnly row
-  Console.info $ show user
+      pure . fmap Sql.fromOnly $ Maybe.listToMaybe rows
 
-  pure
-    . Common.statusResponse Http.found302
-    . Map.insert Http.hLocation "/"
-    . Map.insert
-        Http.hSetCookie
-        (mconcat
-          [ "guid="
-          , Uuid.toASCIIBytes $ Guid.toUuid guid
-          , "; HttpOnly; Path=/; SameSite=Strict"
-          , if Common.isSecure config then "; Secure" else ""
-          ]
-        )
-    $ Common.defaultHeaders config
+makeCookie :: Guid.Guid -> App.App request ByteString.ByteString
+makeCookie guid = do
+  config <- Reader.asks Context.config
+  pure $ mconcat
+    [ "guid="
+    , Uuid.toASCIIBytes $ Guid.toUuid guid
+    , "; HttpOnly; Path=/; SameSite=Strict"
+    , if Common.isSecure config then "; Secure" else ""
+    ]
