235dc1c83460d20c161a1ba4005929c37beb620a
Taylor Fausak <taylor@fausak.me>
2020-06-15 22:02:49 -0400

    Link to GitHub's OAuth application page

--- src/lib/Monadoc/Handler/Account.hs
+++ src/lib/Monadoc/Handler/Account.hs
@@ -5,10 +5,13 @@ where
 
 import qualified Control.Monad.Trans.Reader as Reader
 import qualified Data.Map as Map
+import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
+import qualified Lucid as Html
 import qualified Monadoc.Handler.Index as Index
 import qualified Monadoc.Server.Common as Common
 import qualified Monadoc.Type.App as App
+import qualified Monadoc.Type.Config as Config
 import qualified Monadoc.Type.Context as Context
 import qualified Network.HTTP.Types as Http
 import qualified Network.Wai as Wai
@@ -18,10 +21,22 @@ handle = do
   config <- Reader.asks Context.config
   let headers = Common.defaultHeaders config
   maybeUser <- Index.getCookieUser
-  case maybeUser of
-    Nothing -> do
-      loginUrl <- Index.makeLoginUrl
-      pure
-        . Common.statusResponse Http.found302
-        $ Map.insert Http.hLocation (Text.encodeUtf8 loginUrl) headers
-    Just _ -> pure $ Common.statusResponse Http.ok200 headers
+  loginUrl <- Index.makeLoginUrl
+  pure $ case maybeUser of
+    Nothing -> Common.statusResponse Http.found302
+      $ Map.insert Http.hLocation (Text.encodeUtf8 loginUrl) headers
+    Just _ -> Common.htmlResponse Http.ok200 headers
+      . Index.makeHtmlWith config maybeUser loginUrl
+      . Html.p_
+      $ do
+        "You are logged in. You can manage your "
+        Html.a_
+          [ Html.href_
+          . Text.pack
+          $ "https://github.com/settings/connections/applications/"
+          <> Config.clientId config
+          ]
+          "OAuth application"
+        " access on GitHub. Or you can "
+        Html.a_ [Html.href_ "#todo"] "log out"
+        "."
