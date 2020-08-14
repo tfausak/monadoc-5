b83a1ed0298366e342c936ddb3608b42ce8c39ec
Taylor Fausak <taylor@fausak.me>
2020-08-14 16:44:35 -0400

    Fix Brittany newline conversion

--- src/script/brittany.hs
+++ src/script/brittany.hs
@@ -1,8 +1,6 @@
 #! /usr/bin/env stack
 -- stack exec --package Glob --package process runghc
 module Main where
-import qualified Control.Monad as Monad
-import qualified Data.List as List
 import qualified System.FilePath.Glob as Glob
 import qualified System.IO as IO
 import qualified System.Process as Process
@@ -10,16 +8,20 @@ import qualified System.Process as Process
 main :: IO ()
 main = do
   files <- Glob.glob "src/**/*.hs"
-  Monad.forM_ files $ \file -> do
-    contents <- IO.withFile file IO.ReadMode $ \handle -> do
-      IO.hSetNewlineMode handle IO.universalNewlineMode
-      contents <- IO.hGetContents handle
-      putStrLn $ unwords [file, show $ length contents]
-      pure contents
-    IO.withFile file IO.WriteMode $ \handle -> do
-      IO.hSetNewlineMode handle IO.noNewlineTranslation
-      IO.hPutStr handle contents
+  mapM_ (convertNewlinesTo IO.LF) files
   Process.callProcess "brittany"
     $ "--config-file=config/brittany.yaml"
     : "--write-mode=inplace"
     : files
+  mapM_ (convertNewlinesTo IO.nativeNewline) files
+
+convertNewlinesTo :: IO.Newline -> FilePath -> IO ()
+convertNewlinesTo newline file = do
+  contents <- IO.withFile file IO.ReadMode $ \handle -> do
+    IO.hSetNewlineMode handle IO.universalNewlineMode
+    contents <- IO.hGetContents handle
+    () <- seq (length contents) $ pure ()
+    pure contents
+  IO.withFile file IO.WriteMode $ \handle -> do
+    IO.hSetNewlineMode handle $ IO.NewlineMode newline newline
+    IO.hPutStr handle contents
